cf239f00affbf7cc463275ffefe4e83054d89257
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-07-31 22:25:45 +0200

    fix: prevent No 'Access-Control-Allow-Origin' header at client

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+//prevent No 'Access-Control-Allow-Origin' header
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(require('./src/routing'));
 
 app.listen(PORT, HOST);
